274f627303c7af0cfd25a08ec6bf5ce5ce7737b9
Aldo Diaz <asaavedrajob@gmail.com>
2021-08-08 11:41:51 -0500

    Update template-privacy-policy.php
    - Update comments

--- template-privacy-policy.php
+++ template-privacy-policy.php
@@ -1,7 +1,9 @@
 <?php 
+  // In order to have this template available in our Page Editor, we must add the following comment => Template n(a)me: <the name we want to assign to the template, this could be any but it is recommendable to assign one that it is accordly to the page in which will be used>
   // Template Name: Page Privacy Policy
   get_header(); 
 
+  // get_fields() -> is a method given by Advanced Custom Fields plugin and this will allow us to bring the custom fields we created for this Page Template
   $fields = get_fields();
 ?>
 
